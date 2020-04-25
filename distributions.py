feeda9a54f44baf1581330b42869f498d9e70515
Sharon Wu <wu.sharonsy@gmail.com>
2020-04-25 10:06:25 -0600

    deleted distributions file

--- distributions.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from scipy.special import logit
-from scipy.stats import norm
-
-class LogitNormal:
-    def _pdf(self, x, **kwargs):
-        return norm.pdf(logit(x), **kwargs)/(x*(1-x))
